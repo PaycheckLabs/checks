4ca4972500bc9f7e738bac8cc848058230066f8b
James L. Odom <james@paycheck.io>
2026-02-14 16:22:24 -0800

    Create types.ts

---
+++ packages/sdk/src/types.ts
@@ -0,0 +1,37 @@
+export type Address = `0x${string}`;
+export type Hash = `0x${string}`;
+export type Bytes32 = `0x${string}`;
+
+export type PaymentCheckStatus = 'ACTIVE' | 'REDEEMED' | 'VOID';
+
+export type PaymentCheckEvent =
+  | {
+      type: 'PaymentCheckMinted';
+      checkId: bigint;
+      issuer: Address;
+      initialHolder: Address;
+      token: Address;
+      amount: bigint;
+      claimableAt: bigint;
+      reference: Bytes32;
+      blockNumber: bigint;
+      txHash: Hash;
+    }
+  | {
+      type: 'PaymentCheckRedeemed';
+      checkId: bigint;
+      redeemer: Address;
+      token: Address;
+      amount: bigint;
+      blockNumber: bigint;
+      txHash: Hash;
+    }
+  | {
+      type: 'PaymentCheckVoided';
+      checkId: bigint;
+      issuer: Address;
+      token: Address;
+      amount: bigint;
+      blockNumber: bigint;
+      txHash: Hash;
+    };
