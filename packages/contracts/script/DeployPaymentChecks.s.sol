c9cf09b17de09190dd2e635d1d7e089e58456a4e
James L. Odom <james@paycheck.io>
2026-02-17 17:18:43 -0800

    deploy: add Amoy chain guard to deploy scripts

--- packages/contracts/script/DeployPaymentChecks.s.sol
+++ packages/contracts/script/DeployPaymentChecks.s.sol
@@ -11,6 +11,8 @@ import { PaymentChecks } from "../src/PaymentChecks.sol";
 /// Example:
 /// forge script script/DeployPaymentChecks.s.sol:DeployPaymentChecks --rpc-url $AMOY_RPC_URL --broadcast
 contract DeployPaymentChecks is Script {
+    uint256 internal constant AMOY_CHAIN_ID = 80002;
+
     string internal constant NAME = "Payment Checks";
     string internal constant SYMBOL = "pCHECK";
     string internal constant BASE_URI = "https://checks.xyz/metadata/payment-checks/";
@@ -18,6 +20,8 @@ contract DeployPaymentChecks is Script {
     event Deployed(address indexed paymentChecks);
 
     function run() external returns (PaymentChecks checks) {
+        require(block.chainid == AMOY_CHAIN_ID, "Wrong chain: expected Polygon Amoy (80002)");
+
         uint256 deployerKey = vm.envUint("PRIVATE_KEY");
         address deployer = vm.addr(deployerKey);
 
@@ -25,9 +29,7 @@ contract DeployPaymentChecks is Script {
         console2.log("Chain ID:", block.chainid);
 
         vm.startBroadcast(deployerKey);
-
         checks = new PaymentChecks(NAME, SYMBOL, BASE_URI);
-
         vm.stopBroadcast();
 
         console2.log("PaymentChecks deployed at:", address(checks));
