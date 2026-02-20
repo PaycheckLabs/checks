66f0eb87dd421fb49139c5255c0e00520c1c4f31
James L. Odom <james@paycheck.io>
2026-02-20 15:46:50 -0800

    test: expand PaymentChecks additional coverage (M1 flows)

--- packages/contracts/test/PaymentChecks.additional.t.sol
+++ packages/contracts/test/PaymentChecks.additional.t.sol
@@ -5,9 +5,9 @@ import "forge-std/Test.sol";
 
 import {PaymentChecks} from "../src/PaymentChecks.sol";
 import {IPaymentChecks} from "../src/IPaymentChecks.sol";
+
 import {MockERC20} from "./MockERC20.sol";
 import {SafeERC20} from "../src/vendor/openzeppelin/token/ERC20/utils/SafeERC20.sol";
-
 import {NoReturnERC20, FalseReturnERC20, ReentrantERC20} from "./helpers/EdgeCaseERC20s.sol";
 
 contract PaymentChecksAdditionalTest is Test {
@@ -51,6 +51,7 @@ contract PaymentChecksAdditionalTest is Test {
         uint256 checkId = checks.mintPaymentCheck(recipient, address(token), AMOUNT, 0, ref);
 
         assertEq(checkId, 1);
+
         IPaymentChecks.PaymentCheck memory pc = checks.getPaymentCheck(checkId);
         assertEq(pc.referenceId, ref);
     }
@@ -63,6 +64,77 @@ contract PaymentChecksAdditionalTest is Test {
         assertTrue(_startsWith(uri, "ipfs://checks/"));
     }
 
+    // ---- M1 flow coverage (matches DeployAndSmoke expectations) ----
+
+    function testTransferThenRedeemByNewOwnerWorks() public {
+        vm.prank(issuer);
+        uint256 checkId = checks.mintPaymentCheck(recipient, address(token), AMOUNT, 0, bytes32(0));
+
+        vm.prank(recipient);
+        checks.transferFrom(recipient, otherHolder, checkId);
+
+        assertEq(checks.ownerOf(checkId), otherHolder);
+
+        uint256 beforeBal = token.balanceOf(otherHolder);
+
+        vm.prank(otherHolder);
+        checks.redeemPaymentCheck(checkId);
+
+        assertEq(uint256(checks.getPaymentCheckStatus(checkId)), uint256(IPaymentChecks.Status.REDEEMED));
+        assertEq(token.balanceOf(otherHolder), beforeBal + AMOUNT);
+        assertEq(token.balanceOf(address(checks)), 0);
+    }
+
+    function testPostDatedRedeemRevertsUntilClaimableThenSucceeds() public {
+        uint64 claimableAt = uint64(START_TS + 500);
+
+        vm.prank(issuer);
+        uint256 checkId = checks.mintPaymentCheck(recipient, address(token), AMOUNT, claimableAt, bytes32(0));
+
+        vm.prank(recipient);
+        vm.expectRevert(
+            abi.encodeWithSelector(IPaymentChecks.NotClaimableYet.selector, claimableAt, uint64(START_TS))
+        );
+        checks.redeemPaymentCheck(checkId);
+
+        vm.warp(claimableAt);
+
+        vm.prank(recipient);
+        checks.redeemPaymentCheck(checkId);
+
+        assertEq(uint256(checks.getPaymentCheckStatus(checkId)), uint256(IPaymentChecks.Status.REDEEMED));
+        assertEq(token.balanceOf(address(checks)), 0);
+    }
+
+    function testVoidReturnsFundsAndLocksRedeemForever() public {
+        uint64 claimableAt = uint64(START_TS + 500);
+
+        uint256 issuerBalBeforeMint = token.balanceOf(issuer);
+
+        vm.prank(issuer);
+        uint256 checkId = checks.mintPaymentCheck(recipient, address(token), AMOUNT, claimableAt, bytes32(0));
+
+        // After mint, issuer should be down by AMOUNT and escrow should hold AMOUNT
+        assertEq(token.balanceOf(address(checks)), AMOUNT);
+        assertEq(token.balanceOf(issuer), issuerBalBeforeMint - AMOUNT);
+
+        vm.prank(issuer);
+        checks.voidPaymentCheck(checkId);
+
+        // After void, escrow is cleared and issuer is fully refunded to the pre-mint balance
+        assertEq(uint256(checks.getPaymentCheckStatus(checkId)), uint256(IPaymentChecks.Status.VOID));
+        assertEq(token.balanceOf(address(checks)), 0);
+        assertEq(token.balanceOf(issuer), issuerBalBeforeMint);
+
+        // Even after claimableAt, redeem must fail because status is VOID
+        vm.warp(claimableAt + 1);
+        vm.prank(recipient);
+        vm.expectRevert(
+            abi.encodeWithSelector(IPaymentChecks.CheckNotActive.selector, checkId, IPaymentChecks.Status.VOID)
+        );
+        checks.redeemPaymentCheck(checkId);
+    }
+
     function testVoidStillWorksAfterTransferBeforeClaimableAt() public {
         uint64 claimableAt = uint64(START_TS + 500);
 
@@ -72,6 +144,7 @@ contract PaymentChecksAdditionalTest is Test {
         // transfer NFT to someone else
         vm.prank(recipient);
         checks.transferFrom(recipient, otherHolder, checkId);
+
         assertEq(checks.ownerOf(checkId), otherHolder);
 
         // issuer can still void while post-dated
@@ -89,6 +162,8 @@ contract PaymentChecksAdditionalTest is Test {
         );
         checks.redeemPaymentCheck(checkId);
     }
+
+    // ---- edge-case ERC20 coverage ----
 
     function testNoReturnTokenIsSupportedBySafeERC20() public {
         NoReturnERC20 t = new NoReturnERC20(6);
@@ -119,7 +194,6 @@ contract PaymentChecksAdditionalTest is Test {
     function testReentrancyDuringTransferFromIsBlocked() public {
         ReentrantERC20 t = new ReentrantERC20(6);
         t.mint(issuer, AMOUNT * 10);
-
         t.setChecks(address(checks));
 
         vm.prank(issuer);
@@ -134,6 +208,7 @@ contract PaymentChecksAdditionalTest is Test {
         bytes memory a = bytes(s);
         bytes memory b = bytes(prefix);
         if (b.length > a.length) return false;
+
         for (uint256 i = 0; i < b.length; i++) {
             if (a[i] != b[i]) return false;
         }
